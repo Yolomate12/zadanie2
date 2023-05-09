7dc1e4723a572e54590d1029517934670aeec15e
Yolomate12 <118884181+Yolomate12@users.noreply.github.com>
2023-05-09 11:35:44 +0200

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,48 @@
+var obr11 = "https://assets.autobuzz.my/wp-content/uploads/2021/11/29231455/2021-Porsche-911-GT3-Malaysia-9.jpg";
+var obr12 = "https://assets.autobuzz.my/wp-content/uploads/2021/11/29231458/2021-Porsche-911-GT3-Malaysia-10.jpg";
+
+var obr21 = "https://cdn.motor1.com/images/mgl/x66XP/s1/4x3/2022-porsche-911-gt3-tail.webp";
+var obr22 = "https://sf2.autojournal.fr/wp-content/uploads/autojournal/2021/02/nowe-porsche-911-gt3-otwarcie-1-1024x683.jpg";
+
+var obr31 = "https://cdni.autocarindia.com/Utils/ImageResizer.ashx?n=https://cdni.autocarindia.com/ExtraImages/20220211112712_Singer_Turbo_Study_2022_side.jpg";
+var obr32 = "https://www.topgear.com/sites/default/files/2022/02/Image06.jpg";
+
+var obr41 = "https://media.wired.com/photos/5c58b32e784b8817e6963f63/191:100/w_1280,c_limit/porsche-carrier-transpo.jpg";
+var obr42 = "https://www.topgear.com/sites/default/files/brightcove/videos/images/posters/1546304439_5990283131001_5989649327001-vs.jpg";
+
+var obrazok1 = document.getElementById("img1");
+var obrazok2 = document.getElementById("img2");
+var obrazok3 = document.getElementById("img3");
+var obrazok4 = document.getElementById("img4");
+
+
+function hoverOn1() {
+    obrazok1.src = obr11
+}
+function hoverDown1() {
+    obrazok1.src = obr12
+}
+
+
+function hoverOn2() {
+    obrazok2.src = obr21
+}
+function hoverDown2() {
+    obrazok2.src = obr22
+}
+
+
+function hoverOn3() {
+    obrazok3.src = obr31
+}
+function hoverDown3() {
+    obrazok3.src = obr32
+}
+
+
+function hoverOn4() {
+    obrazok4.src = obr41
+}
+function hoverDown4() {
+    obrazok4.src = obr42
+}
