36900d1c07ff1f8988d8e8c83fe3b5e3338b57c3
Yolomate12 <118884181+Yolomate12@users.noreply.github.com>
2023-06-02 19:47:38 +0200

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,63 @@
+let start = document.getElementById("win")
+
+
+function cayman() {
+    let form = document.getElementById("p718");
+
+    form.style.display = "block"
+    start.style.display = "none"
+    window.scrollTo(0,0);
+
+
+    return;
+}
+function taycan() {
+    let form = document.getElementById("taycan");
+
+    form.style.display = "block"
+    start.style.display = "none"
+    window.scrollTo(0,0);
+
+
+    return;
+}
+function cayanne() {
+    let form = document.getElementById("cayanne");
+
+    form.style.display = "block"
+    start.style.display = "none"
+    window.scrollTo(0,0);
+
+
+    return;
+}
+function p911gt3() {
+    let form = document.getElementById("pgt3");
+
+    form.style.display = "block"
+    start.style.display = "none"
+    window.scrollTo(0,0);
+
+
+    return;
+}
+function p718gt4() {
+    let form = document.getElementById("pgt4");
+
+    form.style.display = "block"
+    start.style.display = "none"
+    window.scrollTo(0,0);
+
+
+    return;
+}
+function panamera() {
+    let form = document.getElementById("panamera");
+
+    form.style.display = "block"
+    start.style.display = "none"
+    window.scrollTo(0,0);
+
+
+    return;
+}
